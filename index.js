4bbe43cafd6c5960acf36c9d28f6b0091f21b72f
Horst Leung <horst.leung@vdelegate.com.hk>
2018-06-29 13:45:56 +0800

    Init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNWebviewWithRefresh } = NativeModules;
+
+export default RNWebviewWithRefresh;
---
+++ ios/RNWebviewWithRefresh.m
@@ -0,0 +1,13 @@
+
+#import "RNWebviewWithRefresh.h"
+
+@implementation RNWebviewWithRefresh
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
